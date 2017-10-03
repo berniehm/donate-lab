833648c059905285278f3a03fb9bfd1b17769030
berniehm <bernieh1012008@live.co.uk>
2017-10-03 12:02:08 +0100

    donation lab initial commit

---
+++ app/controllers/donations.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Donation = require('../models/donation');
+
+exports.home = {
+
+  handler: function (request, reply) {
+    reply.view('home', { title: 'Make a Donation' });
+  },
+
+};
+
+exports.donate = {
+
+  handler: function (request, reply) {
+    let data = request.payload;
+    data.donor = request.auth.credentials.loggedInUser;
+    const donation = new Donation(data);
+    donation.save().then(newDonation => {
+      reply.redirect('/report');
+    }).catch(err => {
+      reply.redirect('/');
+    });
+  },
+
+};
+
+exports.report = {
+
+  handler: function (request, reply) {
+    Donation.find({}).exec().then(allDonations => {
+      reply.view('report', {
+        title: 'Donations to Date',
+        donations: allDonations,
+      });
+    }).catch(err => {
+      reply.redirect('/');
+    });
+  },
+
+};
---
+++ app/models/donation.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const donationSchema = mongoose.Schema({
+  amount: Number,
+  method: String,
+  donor: String,
+});
+
+const Donation = mongoose.model('Donation', donationSchema);
+module.exports = Donation;
